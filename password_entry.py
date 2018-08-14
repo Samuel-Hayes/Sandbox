9cbe11d025c8bbd9ffb177ee99acd3396e4841e9
Samuel Hayes <sam.hayes1999@gmail.com>
2018-08-14 14:39:06 +1000

    Add password checker

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Samuel Hayes"""
+
+min_length = 6
+password = str(input("Password: "))
+
+while len(password) < min_length:
+    print("Password length too short")
+    password = str(input("Password: "))
+
+print("*" * len(password))
